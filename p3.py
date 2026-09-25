612a29153ed324670eaef5c5cfbc5a1e5f0ddbd2
Sreejajinna <sreejajinnaa@gmail.com>
2026-09-25 18:42:10 +0530

    Update p3.py

---
+++ p3.py
@@ -0,0 +1,49 @@
+'''
+
+-----Relational operators-----
+
+--> is used to check the relation b/w two values or variables
+
+'''
+
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+#a=30
+#b=90
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)'''
+
+-----Relational operators-----
+
+--> is used to check the relation b/w two values or variables
+
+'''
+
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+#a=30
+#b=90
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)'''
+
+-----Relational operators-----
+
+--> is used to check the relation b/w two values or variables
+
+'''
+
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b"))
+#a=30
+#b=90
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
